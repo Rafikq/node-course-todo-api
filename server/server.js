ce217e61c111b8a27b45d8e3c3a9ccb04da4b30c
Rafikq <kru.raf123@gmail.com>
2018-11-30 21:13:53 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ var {
     User
 } = require('./models/user.js');
 
+const {
+    ObjectID
+} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -40,6 +44,26 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     })
 });
+
+// GET /todos/1234556 <- id
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id))
+        return res.status(404).send();
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({
+            todo
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+})
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
