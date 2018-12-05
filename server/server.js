8057ca46909529a3b41f6c01ad2a12cc71d68d07
Rafikq <kru.raf123@gmail.com>
2018-12-05 17:25:47 +0100

    Add DELTET /todo route

--- server/server.js
+++ server/server.js
@@ -66,6 +66,20 @@ app.get('/todos/:id', (req, res) => {
 
 })
 
+app.delete('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid)
+        return res.status(404).send();
+    
+    Todo.findByIdAndRemove(id).then((doc)=>{
+        if(!doc)
+            return res.status(404).send();
+        res.send(doc);
+    },(e)=>{
+        res.status(400).send();
+    });
+})
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 })
